fe6c0c994c98de9df31d1f02fdd1971edf83172f
ankur <ankurjana905@gmail.com>
2023-02-25 18:17:56 +0530

    bug fixed

--- app/src/main/java/com/yantrikbits/brodcaster/MainActivity.kt
+++ app/src/main/java/com/yantrikbits/brodcaster/MainActivity.kt
@@ -4,21 +4,19 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.bluetooth.BluetoothAdapter
 import android.content.Intent
-import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.service.autofill.OnClickAction
-import android.view.View.OnClickListener
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
-import androidx.core.app.ActivityCompat
+import com.permissionx.guolindev.PermissionX
 import com.yantrikbits.brodcaster.constants.BluetoothStatus
 import com.yantrikbits.brodcaster.constants.Constant
 
 class MainActivity : AppCompatActivity() {
 
     private var bluetoothStatus: BluetoothStatus = BluetoothStatus.OFF
-    private lateinit var bluetoothOnOffButton: AppCompatButton
+    private lateinit var bluetoothOnButton: AppCompatButton
+    private lateinit var bluetoothOffButton: AppCompatButton
     private var bluetoothAdapter: BluetoothAdapter? = null
     private lateinit var bluetoothEnableIntent: Intent
 
@@ -26,10 +24,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        bluetoothOnOffButton = findViewById(R.id.bluetoothButton)
+        bluetoothOnButton = findViewById(R.id.bluetoothOnButton)
+        bluetoothOffButton = findViewById(R.id.bluetoothOffButton)
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
         bluetoothEnableIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-        onOffBluetooth()
+        permission()
+        onBluetooth()
+        bluetoothOff()
     }
 
     @Deprecated("Deprecated in Java")
@@ -53,48 +54,67 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    private fun onOffBluetooth() {
-        bluetoothOnOffButton.setOnClickListener {
-            if (bluetoothStatus == BluetoothStatus.ON) {
+    @SuppressLint("MissingPermission")
+    private fun onBluetooth() {
+        bluetoothOnButton.setOnClickListener {
+            if (bluetoothAdapter == null) {
                 Toast.makeText(
                     this@MainActivity,
-                    "Bluetooth already On.",
+                    "Your Device NotSupport Bluetooth",
                     Toast.LENGTH_LONG
-                )
-                    .show()
-            } else if (bluetoothStatus == BluetoothStatus.OFF) {
-                if (bluetoothAdapter == null) {
-                    Toast.makeText(
-                        this@MainActivity,
-                        "Your Device NotSupport Bluetooth",
-                        Toast.LENGTH_LONG
-                    ).show()
-                } else {
-                    if (!bluetoothAdapter!!.isEnabled) {
-                        startActivityForResult(
-                            bluetoothEnableIntent,
-                            Constant.REQUEST_CODE_FOR_BLUETOOTH_ENABLE
-                        )
-                    }
-                }
+                ).show()
             } else {
-                if (bluetoothAdapter!!.isEnabled) {
-                    if (ActivityCompat.checkSelfPermission(
-                            this,
-                            Manifest.permission.BLUETOOTH_CONNECT
-                        ) != PackageManager.PERMISSION_GRANTED
-                    ) {
-                        // TODO: Consider calling
-                        //    ActivityCompat#requestPermissions
-                        // here to request the missing permissions, and then overriding
-                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                        //                                          int[] grantResults)
-                        // to handle the case where the user grants the permission. See the documentation
-                        // for ActivityCompat#requestPermissions for more details.
-                    }
-                    bluetoothAdapter!!.disable()
+                if (!bluetoothAdapter!!.isEnabled) {
+                    startActivityForResult(
+                        bluetoothEnableIntent,
+                        Constant.REQUEST_CODE_FOR_BLUETOOTH_ENABLE
+                    )
                 }
+            }
+            bluetoothStatus = BluetoothStatus.ON
+        }
+    }
+
+    private fun bluetoothOff() {
+        bluetoothOffButton.setOnClickListener {
+            if (bluetoothAdapter!!.isEnabled) {
+                bluetoothAdapter!!.disable()
+                bluetoothStatus = BluetoothStatus.OFF
+                Toast.makeText(
+                    this@MainActivity,
+                    "Bluetooth off",
+                    Toast.LENGTH_LONG
+                ).show()
             }
         }
     }
+
+    private fun permission() {
+        PermissionX.init(this)
+            .permissions(
+                Manifest.permission.BLUETOOTH,
+                Manifest.permission.BLUETOOTH_CONNECT,
+                Manifest.permission.BLUETOOTH_ADMIN
+            )
+            .onExplainRequestReason { scope, deniedList ->
+                scope.showRequestReasonDialog(
+                    deniedList,
+                    "Core fundamental are based on these permissions",
+                    "OK",
+                    "Cancel"
+                )
+            }
+            .request { allGranted, grantedList, deniedList ->
+                if (allGranted) {
+                    Toast.makeText(this, "All permissions are granted", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(
+                        this,
+                        "These permissions are denied: $deniedList",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+    }
+
 }
